2abc8c6ea6ad549817b8b0dcd218a9665d213689
abdlmobdy868-design <abdlmobdy868@gmail.com>
2026-07-16 22:36:45 +0300

    Create Course class for student system
    Added Course class with properties for course details.

---
+++ prog.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<StudentCourse> CourseEnrollments { get; set; }
+        public virtual ICollection<Resource> Resources { get; set; }
+        public virtual ICollection<HomeworkSubmission> HomeworkSubmissions { get; set; }
+    }
+}
